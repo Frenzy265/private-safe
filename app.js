81a1c561436c5d710d1db2e2069639f0881f358a
Frenzy265 <klaus_franziska@gmx.de>
2020-11-10 15:08:34 +0100

    Add db.json

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var inquirer = require("inquirer");
+const fs = require("fs").promises;
 
 console.log("Hi, welcome to the password manager");
 
@@ -7,28 +8,46 @@ const secretPassword = "abc";
 var questions = [
   {
     type: "input",
-    name: "name",
-    message: "Enter your Name",
-  },
-  {
-    type: "input",
     name: "masterPassword",
     message: "Enter your master password",
   },
 ];
 
-inquirer.prompt(questions).then((answers) => {
-  const { masterPassword } = answers;
+async function validateAccess() {
+  const { masterPassword } = await inquirer.prompt(questions);
 
-  if (masterPassword === secretPassword) {
-    console.log("You are right and will get access");
-  } else {
+  const passwordSafeJSON = await fs.readFile(
+    "./db.json",
+    "utf8",
+    (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+
+  const passwordSafe = JSON.parse(passwordSafeJSON);
+
+  if (masterPassword !== secretPassword) {
     console.error("You are a intruder");
+    validateAccess();
+    return;
   }
-});
 
-// // const args = process.argv.slice(2);
-// // const passwordName = args[0];
+  const args = process.argv.slice(2);
+  const passwordName = args[0];
+  console.log(`You want to know the password of ${passwordName}?`);
+
+  const password = passwordSafe[passwordName];
+  if (password) {
+    console.log(`Password is ${password}`);
+  } else {
+    console.log("unknown password");
+  }
+}
+validateAccess();
+
 // // console.log(`You want to know the password of ${passwordName}? Please enter the right access key:`);
 
 // // if (passwordName === "Lisa") {
