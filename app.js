7eeec0a25c643cc07a1ae63eafd7bd0ebb54022d
Frenzy265 <klaus_franziska@gmx.de>
2020-11-10 13:25:01 +0100

    Ask fpr master password

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
-console.log("Hello Franzi!");
+var inquirer = require("inquirer");
 
-const args = process.argv.slice(2);
-const passwordName = args[0];
-console.log(`You want to know the password of ${passwordName}?`);
+console.log("Hi, welcome to the password manager");
 
-if (passwordName === "Lisa") {
-  console.log("Password is München");
-} else {
-  console.log("Unknown password");
-}
+const secretPassword = "abc";
+
+var questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter your Name",
+  },
+  {
+    type: "input",
+    name: "masterPassword",
+    message: "Enter your master password",
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  const { masterPassword } = answers;
+
+  if (masterPassword === secretPassword) {
+    console.log("You are right and will get access");
+  } else {
+    console.error("You are a intruder");
+  }
+});
+
+// // const args = process.argv.slice(2);
+// // const passwordName = args[0];
+// // console.log(`You want to know the password of ${passwordName}? Please enter the right access key:`);
+
+// // if (passwordName === "Lisa") {
+// //   console.log("Password is München");
+// // } else {
+// //   console.log("Unknown password");
+// // }
+
+// var questions = [
+//   {
+//     type: "input",
+//     name: "name",
+//     message: "Enter your name",
+//     validate: function (input) {
+//       var correct = input.match("Franzi");
+//       if (correct) {
+//         return "This is the rigth answer";
+//       }
+//       {
+//         return "You are a intruder";
+//       }
+//     },
+//   },
+// ];
